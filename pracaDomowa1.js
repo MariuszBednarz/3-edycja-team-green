541977a319e1aafb67febe549fc925025828ed66
ostrach <ostrach109@gmail.com>
2022-12-24 13:05:20 +0100

    5

--- pracaDomowa1.js
+++ pracaDomowa1.js
@@ -1,6 +1,30 @@
+//TABLICA TESTOWA
+const people = [
+  {
+    firstName: "Bartolomea",
+    lastName: "Lozano",
+    nickname: "Rabona",
+  },
+  {
+    firstName: "Bart",
+    lastName: "Loz",
+    nickname: "Rabo",
+  },
+  {
+    firstName: "Mateuszek",
+    lastName: "Lozeeeen",
+    nickname: "Teoazo",
+  },
+  {
+    firstName: "Mat",
+    lastName: "Loz",
+    nickname: "Teo",
+  },
+];
+
 //  DANE WEJŚCIOWE
 
-// const people = [
+//const people = [
 //   {
 //     firstName: "Bartolomeo",
 //     lastName: "Lozano",
@@ -10,40 +34,39 @@
 //     lastName: "Loza",
 //   },
 // ];
+function createNickname(object) {
+  const takeLetter = object.map((a, index) => {
+    let fName = object[index].firstName
+      .substr(0, 3)
+      .split("")
+      .reverse()
+      .join("")
+      .toLowerCase();
 
-// function createNickname(object) {
-//   const takeLetter = object.map((a, index) => {
-//     let fName = object[index].firstName
-//       .substr(0, 3)
-//       .split("")
-//       .reverse()
-//       .join("")
-//       .toLowerCase();
+    let lName = object[index].lastName
+      .substr(object[index].lastName.length - 3)
+      .split("")
+      .reverse()
+      .join("")
+      .toLowerCase();
 
-//     let lName = object[index].lastName
-//       .substr(object[index].lastName.length - 3)
-//       .split("")
-//       .reverse()
-//       .join("")
-//       .toLowerCase();
+    let nickname = fName.concat(lName);
 
-//     let nickname = fName.concat(lName);
+    const firstLetter = nickname?.slice(0, 1)?.toUpperCase();
+    const restOfTheWord = nickname.slice(1);
 
-//     const firstLetter = nickname?.slice(0, 1)?.toUpperCase();
-//     const restOfTheWord = nickname.slice(1);
+    return firstLetter + restOfTheWord;
+  });
 
-//     return firstLetter + restOfTheWord;
-//   });
+  for (number in object) {
+    object[number].nickname = takeLetter[number];
+  }
+  return object;
+}
 
-//   for (number in object) {
-//     object[number].nickname = takeLetter[number];
-//   }
-//   return object;
-// }
+const result = createNickname(people);
 
-// const result = createNickname(people);
-
-// console.log(result);
+//console.log(result);
 
 /* 
         1. Napisz funkcję mapującą, która utworzy klucz(właściwość) nickname na każdej osobie w tablicy w następujący sposób:
@@ -75,18 +98,18 @@
     */
 
 //  DANE WEJŚCIOWE
-const people = [
-  {
-    firstName: "Bartolomeo",
-    lastName: "Lozano",
-    nickname: "Rabona",
-  },
-  {
-    firstName: "Mateo",
-    lastName: "Loza",
-    nickname: "Teoazo",
-  },
-];
+// const people = [
+//   {
+//     firstName: "Bartolomeo",
+//     lastName: "Lozano",
+//     nickname: "Rabona",
+//   },
+//   {
+//     firstName: "Mateo",
+//     lastName: "Loza",
+//     nickname: "Teoazo",
+//   },
+// ];
 
 for (number in people) {
   people[number].introduceYourself = function introduceYourself() {
@@ -95,7 +118,7 @@ for (number in people) {
   };
 }
 
-// people.forEach((object) => console.log(object.introduceYourself()));
+//people.forEach((object) => console.log(object.introduceYourself()));
 
 // console.log(people[0].introduceYourself());
 
@@ -137,34 +160,34 @@ for (number in people) {
 
 const colors = ["red", "green", "yellow", "blue", "pink", "orange"];
 
-// for (number in people) {
-//   people[number].getFavouriteColor = function getFavouriteColor(number) {
-//     if (number >= 1 && number <= 30) {
-//       colorIndex =
-//         (this.firstName.length +
-//           this.lastName.length +
-//           this.nickname.length -
-//           number) %
-//         colors.length;
+for (number in people) {
+  people[number].getFavouriteColor = function getFavouriteColor(number) {
+    if (number >= 1 && number <= 30) {
+      colorIndex =
+        (this.firstName.length +
+          this.lastName.length +
+          this.nickname.length -
+          number) %
+        colors.length;
 
-//       return colors[Math.abs(colorIndex)];
-//     } else if (number === undefined) {
-//       number = 5;
-//       colorIndex =
-//         (this.firstName.length +
-//           this.lastName.length +
-//           this.nickname.length -
-//           number) %
-//         colors.length;
+      return colors[Math.abs(colorIndex)];
+    } else if (number === undefined) {
+      number = 5;
+      colorIndex =
+        (this.firstName.length +
+          this.lastName.length +
+          this.nickname.length -
+          number) %
+        colors.length;
 
-//       return colors[Math.abs(colorIndex)];
-//     } else if (number < 1) {
-//       return "podałeś za małą liczbę, liczba nie może być mniejsza niż 1";
-//     } else {
-//       return "podałeś za dużą liczbę, liczba nie może być większa niż 30";
-//     }
-//   };
-// }
+      return colors[Math.abs(colorIndex)];
+    } else if (number < 1) {
+      return "podałeś za małą liczbę, liczba nie może być mniejsza niż 1";
+    } else {
+      return "podałeś za dużą liczbę, liczba nie może być większa niż 30";
+    }
+  };
+}
 
 //console.log(people[0].getFavouriteColor(5));
 
@@ -227,9 +250,9 @@ function getFavouriteColor(number, index) {
   }
 }
 
-const checkPerson = getFavouriteColor(5, 0);
+// const checkPerson = getFavouriteColor(5, 0);
 
-console.log(checkPerson);
+// console.log(checkPerson);
 
 /*
         5. Zadanie polega na użyciu .filter() .map() .reduce w wersji łańcuchowej,
@@ -268,6 +291,51 @@ console.log(checkPerson);
         imię, którego chociaż jedna litera jest większa >= s
         g) posortuj tablicę alfabetycznie
     */
+
+function primeCheck(num) {
+  if (num <= 1) return false;
+  if (num % 2 == 0 && num > 2) return false;
+  const s = Math.sqrt(num);
+  for (let i = 3; i <= s; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+const randomNumber = Math.floor(Math.random() * 101) + 1;
+const isElite = primeCheck(randomNumber);
+
+function selection(element) {
+  if (isElite == true) {
+    return element;
+  } else {
+    return (
+      element.lastName.length &&
+      element.nickname.includes("a") &&
+      (element.firstName[element.firstName.length - 1] === "k" ||
+        element.firstName[element.firstName.length - 1] === "a")
+    );
+  }
+}
+function resversObject(object) {
+  const shift = [];
+  for (element in object) {
+    if (typeof object[element] !== "function") {
+      shift[object[element]] = element;
+    }
+  }
+  return shift;
+}
+const filteredPeople = people
+  .filter(selection)
+  .map(resversObject)
+  .reduce((acc, next) => {
+    for (element in next) {
+      acc[element] = next[element];
+    }
+    return acc;
+  }, {});
+
+//console.log(filteredPeople);
 
 /*
         *6. Currying function
